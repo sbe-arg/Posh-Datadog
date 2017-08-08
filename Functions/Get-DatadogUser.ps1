57c23077e82a5b1dff5bcea3a9586778dfb4f833
Santiago Bernhardt <santiago.bernhardt@xero.com>
2017-08-09 09:45:05 +1200

    dont be destructive to users
    add get and edit user

---
+++ Functions/Get-DatadogUser.ps1
@@ -0,0 +1,36 @@
+<#
+.SYNOPSIS
+    Connect to Datadog API pull users
+.DESCRIPTION
+    Get users from datadog
+.EXAMPLE
+    Get-DatadogUser -Filter "valuehere"
+    Get-DatadogUser -Backup
+#>
+
+function Get-DatadogUser {
+  param(
+      [Parameter(Mandatory=$false)]
+      [string]$Api_Key = $env:Datadog_API_Key,
+      [string]$App_Key = $env:Datadog_APP_Key,
+      [string]$Filter,
+      [switch]$Backup
+  )
+
+    $url = "https://app.datadoghq.com/api/v1/user?api_key=$env:Datadog_API_Key&application_key=$env:Datadog_APP_Key"
+
+    $results = Invoke-RestMethod -Uri $url -Method Get
+    if(-NOT $Filter){
+      $results
+    }
+    else{
+      $results = $results | select -expandproperty * | where {$_.Handle -like "*$Filter*"}
+      $results
+    }
+
+    if($Backup){
+      $folder = Read-Host = "Destination folder"
+      foreach ($r in $results){$r | ConvertTo-Json | Out-File -FilePath $folder\$($r.id).json}
+    }
+
+}
---
+++ Functions/Set-DatadogUser.ps1
@@ -0,0 +1,59 @@
+<#
+.SYNOPSIS
+    Connect to Datadog API edit users
+.DESCRIPTION
+    Edit users from datadog
+.EXAMPLE
+    Set-DatadogUser -User "fullemailhere"
+    Set-DatadogUser -SetRole "adm"
+    Set-DatadogUser -SetName "sting"
+    Set-DatadogUser -SetEmail "string"
+#>
+
+function Set-DatadogUser {
+  param(
+      [Parameter(Mandatory=$false)]
+      [string]$Api_Key = $env:Datadog_API_Key,
+      [string]$App_Key = $env:Datadog_APP_Key,
+      [Parameter(Mandatory=$true)]
+      [string]$User,
+      [ValidateSet("adm","st","ro")]
+      [string]$SetRole,
+      [string]$SetName,
+      [string]$SetEmail
+
+
+  )
+    $Values = New-Object System.Collections.Generic.List[System.Object]
+    if($SetName){
+      if(-NOT $SetRole -and -NOT $SetEmail){
+        $Name = "`"name`": `"$SetName`""
+        $Values.add($Name)
+      }else{
+        $Name = "`"name`": `"$SetName`",`n"
+        $Values.add($Name)
+      }
+    }
+    if($SetEmail){
+      if(-NOT $SetRole){
+        $Email = "`"email`": `"$SetEmail`""
+        $Values.add($Email)
+      }else{
+        $Email = "`"email`": `"$SetEmail`",`n"
+        $Values.add($Email)
+      }
+    }
+    if($SetRole){
+      $Role = "`"access_role`": `"$SetRole`""
+      $Values.add($Role)
+    }
+
+    Get-DatadogUser -Filter $User
+    $Body = "{`n$Values`n}" | ConvertFrom-Json | ConvertTo-Json
+
+    $url = "https://app.datadoghq.com/api/v1/user/$($User)?api_key=$env:Datadog_API_Key&application_key=$env:Datadog_APP_Key"
+
+    $results = Invoke-RestMethod -Uri $url -Method Put
+    $results
+
+}
